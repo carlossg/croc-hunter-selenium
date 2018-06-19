10e7c58d68c7bae3bde73615a551cddaa5c9ce8f
Carlos Sanchez <carlos@apache.org>
2018-06-19 15:58:05 +0200

    Comment out some tests

--- src/test/java/org/csanchez/selenium/example/ExampleTest.java
+++ src/test/java/org/csanchez/selenium/example/ExampleTest.java
@@ -46,11 +46,11 @@ public class ExampleTest {
 
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return ! d.findElement(By.name("release")).getText().isEmpty();
-            }
-        });
+        // (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+        //     public Boolean apply(WebDriver d) {
+        //         return ! d.findElement(By.name("release")).getText().isEmpty();
+        //     }
+        // });
 
         Thread.sleep(SLEEP);
     }
